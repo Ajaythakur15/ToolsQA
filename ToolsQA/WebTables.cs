4997f7d6342124e5f5e14a4920ba5c94fb4ebb9a
PrachiSharma444 <prachish74@gmail.com>
2024-05-15 16:26:31 +0530

    WebTables modified

--- ToolsQA/WebTables.cs
+++ ToolsQA/WebTables.cs
@@ -21,70 +21,76 @@ namespace ToolQAPOC
             ScrollPage(500);
         }
 
-        //[Test]
-        //public void ClickAddButton()
-        //{
-        //    ClickElement(By.XPath("//button[@id='addNewRecordButton']"));
-        //    Assert.IsTrue(IsElementDisplayed(By.XPath("//button[contains(text(),'Add')]")), "Add button should be clicked");
-        //}
+        [Test]
+        public void CheckSubmitBtnDisplayed()
+        {
+            ClickAddButton();
+            Assert.IsTrue(SubmitButtonDisplayed(),"Submit button is not Displayed");
+        }
 
         [Test]
         public void TestTextBoxValues()
         {
             ClickAddButton();
-            EnterValuesIntoTextbox("firstName", "Prachi");
-            EnterValuesIntoTextbox("lastName", "Sharma");
-            EnterValuesIntoTextbox("userEmail", "prachi@gmail.com");
-            EnterValuesIntoTextbox("age", "22");
-            EnterValuesIntoTextbox("salary", "2200");
-            EnterValuesIntoTextbox("department", "MCA");
-            SubmitButtonDisplayed();
-            //ClickOnClickButton();
-            Assert.IsTrue(SubmitButtonDisplayed(), "Submit button should be hidden after submit");
 
-            Assert.AreEqual("Prachi", GetTextBoxValue("firstName"), "Firstname value does not match");
-            Assert.AreEqual("Sharma", GetTextBoxValue("lastName"), "Lastname value does not match");
-            Assert.AreEqual("prachi@gmail.com", GetTextBoxValue("userEmail"), "UserEmail value does not match");
-            Assert.AreEqual("22", GetTextBoxValue("age"), "Age value does not match");
-            Assert.AreEqual("2200", GetTextBoxValue("salary"), "Salary value does not match");
-            Assert.AreEqual("MCA", GetTextBoxValue("department"), "Department value does not match");
-        }
-        [Test]
-        public void TestAddButtonSubmit()
-        {
-            ClickAddButton();
-            EnterValuesIntoTextbox("firstName", "Prachi");
-            // Add explicit wait before interacting with the firstName element
-            WaitForElement(By.CssSelector("#firstName"));
-            EnterValuesIntoTextbox("lastName", "Sharma");
-            EnterValuesIntoTextbox("userEmail", "prachi@gmail.com");
-            EnterValuesIntoTextbox("age", "22");
-            EnterValuesIntoTextbox("salary", "2200");
-            EnterValuesIntoTextbox("department", "MCA");
+            Dictionary<string, string> inputvalues = new Dictionary<string, string>
+            {
+                {"firstName" , "Prachi" },
+                { "lastName", "Sharma"},
+                {"userEmail", "prachi@gmail.com" },
+                { "age", "22"},
+                { "salary", "2200"},
+                 { "department", "MCA" }
+            };
+            int repeatCount = 5;
+            for(int i=0;i< repeatCount; i++)
+            {
+
+            
+            foreach (var values in inputvalues)
+            {
+                EnterValuesIntoTextbox(values.Key, values.Value);
+                Assert.AreEqual(values.Value, GetTextBoxValue(values.Key),$"{values.Key} does not match");
+            }
+            }
+            /* EnterValuesIntoTextbox("firstName", "Prachi");
+             EnterValuesIntoTextbox("lastName", "Sharma");
+             EnterValuesIntoTextbox("userEmail", "prachi@gmail.com");
+             EnterValuesIntoTextbox("age", "22");
+             EnterValuesIntoTextbox("salary", "2200");
+             EnterValuesIntoTextbox("department", "MCA");
+
+
+             Assert.AreEqual("Prachi", GetTextBoxValue("firstName"), "Firstname value does not match");
+             Assert.AreEqual("Sharma", GetTextBoxValue("lastName"), "Lastname value does not match");
+             Assert.AreEqual("prachi@gmail.com", GetTextBoxValue("userEmail"), "UserEmail value does not match");
+             Assert.AreEqual("22", GetTextBoxValue("age"), "Age value does not match");
+             Assert.AreEqual("2200", GetTextBoxValue("salary"), "Salary value does not match");
+             Assert.AreEqual("MCA", GetTextBoxValue("department"), "Department value does not match");
+ */
+
+            //Clicl on submit button
             ClickOnSubmitButton();
-            //Assert.IsTrue(SubmitButtonDisplayed(), "Submit button should be hidden after submit");
+             Assert.IsFalse(SubmitButtonDisplayed(), "Submit button should be hidden after submit");
 
-            Assert.AreEqual("Prachi", GetTextBoxValue("firstName"), "Firstname value does not match");
-            Assert.AreEqual("Sharma", GetTextBoxValue("lastName"), "Lastname value does not match");
-            Assert.AreEqual("prachi@gmail.com", GetTextBoxValue("userEmail"), "UserEmail value does not match");
-            Assert.AreEqual("22", GetTextBoxValue("age"), "Age value does not match");
-            Assert.AreEqual("2200", GetTextBoxValue("salary"), "Salary value does not match");
-            Assert.AreEqual("MCA", GetTextBoxValue("department"), "Department value does not match");
         }
+
 
         [Test]
         public void SearchBox()
         {
             EnterValuesIntoTextbox("searchBox", "Al");
+           
             ClickElement(By.XPath("//span[@id='basic-addon2']"));
-            Assert.IsTrue(IsElementDisplayed(By.XPath("//span[@id='basic-addon2']")), "Search is not happening");
+            
         }
 
         [Test]
         public void DeleteButton()
         {
-            ClickElement(By.XPath("//span[@id='delete-record-2']"));
-            Assert.IsTrue(IsElementDisplayed(By.XPath("//span[@id='delete-record-1']")), "Button should delete");
+
+            Assert.IsTrue(IsElementDisplayed(By.XPath("//span[@id='delete-record-1']")),"Delete button should be displayed");
+            //ClickElement(By.XPath("//span[@id='delete-record-2']"));
         }
 
         [Test]
@@ -95,11 +101,15 @@ namespace ToolQAPOC
 
             EnterValuesIntoTextbox("age", "44");
             EnterValuesIntoTextbox("department", "BCA");
-            ClickElement(By.XPath("//button[@type='submit']"));
-            Assert.IsTrue(SubmitButtonDisplayed(), "Submit is not done");
 
             Assert.AreEqual("44", GetTextBoxValue("age"), "Age value does not match");
             Assert.AreEqual("BCA", GetTextBoxValue("department"), "Department value does not match");
+        
+           
+            Assert.IsTrue(SubmitButtonDisplayed(), "Submit is not Displayed");
+            ClickElement(By.XPath("//button[contains(text(), 'Submit')]"));
+
+
         }
 
         [TearDown]
@@ -110,38 +120,39 @@ namespace ToolQAPOC
 
         public void ClickElement(By locator)
         {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             IWebElement element = wait.Until(ExpectedConditions.ElementToBeClickable(locator));
             element.Click();
         }
 
         public void ClickOnSubmitButton()
         {
-            ClickElement(By.XPath("//button[@id='submit']"));
+            ClickElement(By.XPath("//button[contains(text(), 'Submit')]"));
         }
         public void ClickAddButton()
         {
             ClickElement(By.XPath("//button[@id='addNewRecordButton']"));
         }
 
-        public void EnterValuesIntoTextbox(string name, string text)
+        public void EnterValuesIntoTextbox(string Id, string text)
         {
-            IWebElement element = WaitUntilElementIsVisible(By.Id(name));
+            IWebElement element = driver.FindElement(By.Id(Id));
             element.Clear();
             element.SendKeys(text);
         }
 
-        private string GetTextBoxValue(string name)
+        private string GetTextBoxValue(string Id)
         {
-            IWebElement element = WaitUntilElementIsVisible(By.Id(name));
-            return element.GetAttribute("value");
+            /*IWebElement element = driver.FindElement(By.Id(Id));
+            return element.GetAttribute("value");*/
+            return driver.FindElement(By.Id(Id)).GetAttribute("value");
         }
 
         public bool SubmitButtonDisplayed()
         {
             try
             {
-                return driver.FindElement(By.CssSelector("#submit")).Displayed;
+                return driver.FindElement(By.XPath("//button[@id='submit']")).Displayed;
             }
             catch (NoSuchElementException)
             {
@@ -149,19 +160,10 @@ namespace ToolQAPOC
             }
         }
 
-        private IWebElement WaitForElementClickable(By locator, int timeoutInSeconds = 15)
-        {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
-            return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(locator));
-        }
-        private void WaitForElement(By locator, int timeoutInSeconds = 15)
-        {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
-            wait.Until(ExpectedConditions.ElementExists(locator));
-        }
+       
         private IWebElement WaitUntilElementIsVisible(By locator)
         {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             return wait.Until(ExpectedConditions.ElementIsVisible(locator));
         }
 
