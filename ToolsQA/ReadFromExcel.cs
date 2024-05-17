946588c282794907f1d48d373588dee138f63728
PrachiSharma444 <prachish74@gmail.com>
2024-05-17 17:37:57 +0530

    Read data from excel

---
+++ ToolsQA/ReadFromExcel.cs
@@ -0,0 +1,73 @@
+﻿using OfficeOpenXml;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ToolsQA
+{
+    public class ReadFromexcel
+    {
+
+        public static IEnumerable<object[]> ReadExcel()
+        {
+            //create worksheet object
+            using (ExcelPackage package = new ExcelPackage(new FileInfo("Data.xlsx")))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets["sheet1"];
+                int rowcount = worksheet.Dimension.End.Row;
+                for (int i = 2; i <= rowcount; i++)
+                {
+                    yield return new object[] {
+                    worksheet.Cells[i, 1].Value?.ToString().Trim(), //FirstName
+                    worksheet.Cells[i, 2].Value?.ToString().Trim(),
+                    worksheet.Cells[i, 3].Value?.ToString().Trim(),
+                    worksheet.Cells[i, 4].Value?.ToString().Trim(),
+                    worksheet.Cells[i, 5].Value?.ToString().Trim(),
+                    worksheet.Cells[i, 6].Value?.ToString().Trim()
+                    };
+
+                }
+            }
+
+        }
+
+        [TestCaseSource(nameof(ReadExcel))]
+        [Test]
+        public void DataDrivenUsingExcel(string fname, string lname, string email, string age, string salary, string department)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://demoqa.com/webtables");
+
+            StartPage start = new StartPage(driver);
+            start.ClickAdd();
+
+            driver.FindElement(By.Id("firstName")).SendKeys(fname);
+            driver.FindElement(By.Id("lastName")).SendKeys(lname);
+            driver.FindElement(By.Id("userEmail")).SendKeys(email);
+            driver.FindElement(By.Id("age")).SendKeys(age);
+            driver.FindElement(By.Id("salary")).SendKeys(salary);
+            driver.FindElement(By.Id("department")).SendKeys(department);
+
+            driver.Quit();
+
+        }
+    }
+
+    public class StartPage
+    {
+
+        private IWebDriver driver;
+
+        public StartPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void ClickAdd()
+        {
+
+            driver.FindElement(By.XPath("//button[@id='addNewRecordButton']")).Click();
+        }
+    }
+}      
+
---
+++ ToolsQA/WriteDataInExcel.cs
@@ -0,0 +1,55 @@
+﻿using OfficeOpenXml;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolsQA
+{
+    public class WriteDataInExcel
+    {
+        
+        public void AddDataFromWebpageToExcel()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://demoqa.com/webtables");
+            driver.Manage().Window.Maximize();
+            //ScrollPage(500);
+
+            var cells = driver.FindElements(By.CssSelector("div.rt-td"));
+                string FirstName = cells[0].Text;
+                string LastName = cells[1].Text;
+                string Age = cells[2].Text;
+                string Email = cells[3].Text;
+                string Salary = cells[4].Text;
+                string department = cells[5].Text;
+
+
+
+            using (ExcelPackage package = new ExcelPackage(new FileInfo("Data.xlsx")))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets["sheet2"];
+                int rowcount = worksheet.Dimension.End.Row;
+                for (int i = 2; i <= rowcount; i++)
+                {
+                    worksheet.Cells[i, 1].Value = FirstName;
+                    worksheet.Cells[i, 2].Value = LastName;
+                    worksheet.Cells[i, 3].Value = Age;
+                    worksheet.Cells[i, 4].Value = Email;
+                    worksheet.Cells[i, 5].Value = Salary;
+                    worksheet.Cells[i, 6].Value = department;
+                }
+                package.Save();
+               
+
+            }
+            driver.Quit();
+        }
+    }
+
+
+   
+}
