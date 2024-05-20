a804fb0033f5c6965b072615201f58fd98cdfe00
Ajaythakur15 <tech.ajaythakur@hotmail.com>
2024-05-20 11:44:04 +0530

    test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace FunctionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum();
+            Console.ReadKey();
+        }
+        static void Sum()
+        {
+            int x = 10;
+            int y = 20;
+            int sum = x + y;
+            Console.WriteLine($"Sum of {x} and {y} is {sum}");
+        }
+    }
+}
