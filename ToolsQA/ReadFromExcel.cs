be0481f2bbb65a406f64e3508ac8af0ca94848d0
Ajaythakur15 <tech.ajaythakur@hotmail.com>
2024-05-21 15:29:27 +0530

    Refactor

--- ToolsQA/ReadFromExcel.cs
+++ ToolsQA/ReadFromExcel.cs
@@ -2,50 +2,60 @@
 using OfficeOpenXml;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace ToolsQA
 {
     [TestFixture]
-    public class ReadFromexcel
+    public class ReadFromExcel
     {
         private IWebDriver driver;
+
         [SetUp]
         public void OpenBrowser()
         {
-             driver = new ChromeDriver();
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
         }
 
         public static IEnumerable<object[]> ReadExcel()
         {
-            //create worksheet object
-            using (ExcelPackage package = new ExcelPackage(new FileInfo("Data.xlsx")))
+            // Create worksheet object
+            string filePath = "Data.xlsx";
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"File not found: {filePath}");
+            }
+
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(filePath)))
             {
                 ExcelWorksheet worksheet = package.Workbook.Worksheets["sheet1"];
-                int rowcount = worksheet.Dimension.End.Row;
-                for (int i = 2; i <= rowcount; i++)
+                int rowCount = worksheet.Dimension.End.Row;
+                for (int i = 2; i <= rowCount; i++)
                 {
-                    yield return new object[] {
-                    worksheet.Cells[i, 1].Value?.ToString().Trim(), //FirstName
-                    worksheet.Cells[i, 2].Value?.ToString().Trim(),
-                    worksheet.Cells[i, 3].Value?.ToString().Trim(),
-                    worksheet.Cells[i, 4].Value?.ToString().Trim(),
-                    worksheet.Cells[i, 5].Value?.ToString().Trim(),
-                    worksheet.Cells[i, 6].Value?.ToString().Trim()
+                    yield return new object[]
+                    {
+                        worksheet.Cells[i, 1].Value?.ToString().Trim(), // FirstName
+                        worksheet.Cells[i, 2].Value?.ToString().Trim(), // LastName
+                        worksheet.Cells[i, 3].Value?.ToString().Trim(), // Email
+                        worksheet.Cells[i, 4].Value?.ToString().Trim(), // Age
+                        worksheet.Cells[i, 5].Value?.ToString().Trim(), // Salary
+                        worksheet.Cells[i, 6].Value?.ToString().Trim()  // Department
                     };
-
                 }
             }
         }
+
         [TestCaseSource(nameof(ReadExcel))]
         [Test]
         public void DataDrivenUsingExcel(string fname, string lname, string email, string age, string salary, string department)
         {
-           
-            driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl("https://demoqa.com/webtables");
 
-            StartPage start = new StartPage(driver);
-            start.ClickAdd();
+            StartPage startPage = new StartPage(driver);
+            startPage.ClickAdd();
 
             driver.FindElement(By.Id("firstName")).SendKeys(fname);
             driver.FindElement(By.Id("lastName")).SendKeys(lname);
@@ -54,28 +64,28 @@ namespace ToolsQA
             driver.FindElement(By.Id("salary")).SendKeys(salary);
             driver.FindElement(By.Id("department")).SendKeys(department);
 
-           
-
+            driver.FindElement(By.Id("submit")).Click();
         }
+
         [TearDown]
         public void CloseBrowser()
         {
             driver.Quit();
         }
     }
+
     public class StartPage
     {
-        private IWebDriver driver;
+        private readonly IWebDriver driver;
 
         public StartPage(IWebDriver driver)
         {
             this.driver = driver;
         }
+
         public void ClickAdd()
         {
-
-            driver.FindElement(By.XPath("//button[@id='addNewRecordButton']")).Click();
+            driver.FindElement(By.Id("addNewRecordButton")).Click();
         }
     }
-}      
-
+}
