53998b1bfa4b2ec77f13a1a48b25ccb01c9de024
Ajaythakur15 <tech.ajaythakur@hotmail.com>
2024-05-21 15:05:59 +0530

    Refactor and bug fixes

--- ToolsQA/Upload&Download.cs
+++ ToolsQA/Upload&Download.cs
@@ -1,12 +1,10 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace ToolsQA
 {
@@ -18,30 +16,31 @@ namespace ToolsQA
         [SetUp]
         public void StartBrowser()
         {
-            driver = new ChromeDriver();
+            var options = new ChromeOptions();
+            options.AddUserProfilePreference("download.default_directory", Path.GetFullPath("Downloads"));
+            options.AddUserProfilePreference("download.prompt_for_download", false);
+            options.AddUserProfilePreference("disable-popup-blocking", "true");
+
+            driver = new ChromeDriver(options);
             driver.Navigate().GoToUrl("https://demoqa.com/upload-download");
             driver.Manage().Window.Maximize();
-            ScrollDown(400);
+            ScrollDown(500);
         }
+
         [Test]
         public void ClickDownloadBtn()
         {
-            Assert.IsTrue(IsBtnVisible(),"Button is not displayed");
-            ClickBtn();   
+            Assert.IsTrue(IsBtnVisible(), "Download button is not displayed");
+            ClickBtn();
         }
+
         [Test]
         public void ClickChooseFile()
         {
-            Assert.IsTrue(IsChooseVisible(), "ChooseFilebtn is not displayed");
+            Assert.IsTrue(IsChooseVisible(), "Choose file button is not displayed");
             ClickChooseBtn();
-
         }
 
-        public void ScrollDown(int yOffset)
-        {
-            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
-            js.ExecuteScript($"window.scrollBy(0, {yOffset});");
-        }
         [TearDown]
         public void CloseBrowser()
         {
@@ -52,25 +51,62 @@ namespace ToolsQA
         {
             ClickElement(By.XPath("//a[@id='downloadButton']"));
         }
+
         public bool IsBtnVisible()
         {
-            return driver.FindElement(By.XPath("//a[@id='downloadButton']")).Displayed;
+            return IsElementVisible(By.XPath("//a[@id='downloadButton']"));
         }
+
         public void ClickChooseBtn()
         {
-            ClickElement(By.XPath("//input[@id='uploadFile']"));
+            var uploadElement = driver.FindElement(By.Id("uploadFile"));
+            uploadElement.SendKeys(Path.GetFullPath("C:/QA/Test/File.txt")); // Update the path to your file
+
         }
+
         public bool IsChooseVisible()
         {
-            return driver.FindElement(By.XPath("//input[@id='uploadFile']")).Displayed;
+            return IsElementVisible(By.XPath("//input[@id='uploadFile']"));
         }
+
         public void ClickElement(By locator)
         {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            IWebElement element = wait.Until(ExpectedConditions.ElementToBeClickable(locator));
-            element.Click();
+            try
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                IWebElement element = wait.Until(ExpectedConditions.ElementToBeClickable(locator));
+                element.Click();
+            }
+            catch (WebDriverTimeoutException e)
+            {
+                throw new Exception($"Element with locator: {locator} was not clickable after 10 seconds.", e);
+            }
+            catch (NoSuchElementException e)
+            {
+                throw new Exception($"Element with locator: {locator} was not found.", e);
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Unexpected error occurred while clicking element with locator: {locator}.", e);
+            }
         }
 
+    public bool IsElementVisible(By locator)
+        {
+            try
+            {
+                return driver.FindElement(locator).Displayed;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
 
+        public void ScrollDown(int yOffset)
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript($"window.scrollBy(0, {yOffset});");
+        }
     }
 }
