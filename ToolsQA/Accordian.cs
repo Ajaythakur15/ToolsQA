7ef198e4080eeb4481a0914e7c37bdb1d9f0664d
PrachiSharma444 <prachish74@gmail.com>
2024-05-24 16:37:47 +0530

    Accordian an AutoComple 24/05/2024

---
+++ ToolsQA/Accordian.cs
@@ -0,0 +1,110 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolsQA
+{
+    public class Accordian
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://demoqa.com/accordian");
+            driver.Manage().Window.Maximize();
+            ScrollDown(300);
+        }
+        [Test]
+        public void Section1Heading()
+        {
+            SectionsTestCases obj = new SectionsTestCases(driver);
+            obj.ClickHeading1();
+            Assert.IsTrue(obj.IsHeading1Visible(), "Heading1 is not visible after click");
+        }
+        [Test]       
+        public void Section2Heading()
+        {
+            SectionsTestCases obj = new SectionsTestCases(driver);
+            ScrollDown(300);
+            obj.ClickHeading2();
+            Assert.IsTrue(obj.IsHeading1Visible(), "Heading2 is not visible");
+
+        }
+        [Test]
+        public void Section3Heading()
+        {
+            SectionsTestCases obj = new SectionsTestCases(driver);
+            ScrollDown(300);
+            obj.ClickHeading3();
+            Assert.IsTrue(obj.IsHeading2Visible(), "Heading3 is not visible");
+        }
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Quit();
+        }
+
+        public void ScrollDown(int yOffset)
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript($"window.scrollBy(0,{yOffset});");
+        }
+    }
+
+    public class SectionsTestCases
+    {
+        private IWebDriver driver;
+
+        public SectionsTestCases(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        private By section1headingInput => By.Id("section1Heading");
+        private By section2headingInput => By.Id("section2Heading");
+        private By section3headingInput => By.Id("section3Heading");
+
+
+        public void ClickHeading1()
+        {
+            ClickElement(section1headingInput);
+        }
+        public bool IsHeading1Visible()
+        {
+            return driver.FindElement(By.Id("section1Heading")).Displayed;
+        }
+
+        public void ClickHeading2()
+        {
+            ClickElement(section2headingInput);
+        }
+        public bool IsHeading2Visible()
+        {
+            return driver.FindElement(By.Id("section2Heading")).Displayed;
+        }
+        public void ClickHeading3()
+        {
+            ClickElement(section3headingInput);
+        }
+        public bool IsHeading3Visible()
+        {
+            return driver.FindElement(By.Id("section3Heading")).Displayed;
+        }
+
+        public void ClickElement(By by , int TimeOutInSeconds = 60 )
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(TimeOutInSeconds));
+            IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(by));
+            element.Click();
+        }
+    }
+}
---
+++ ToolsQA/AutoComplete.cs
@@ -0,0 +1,109 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+
+namespace ToolsQA
+{
+    public class AutoComplete
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void OpenBrowser()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://demoqa.com/auto-complete");
+            driver.Manage().Window.Maximize();
+            ScrollDown(300);
+        }
+        [Test]
+        public void MultiColorNames()
+        {
+            MethodsForMulticolour obj = new MethodsForMulticolour(driver);
+            obj.Input1("P","G","B");
+        }
+        [Test]
+        public void SingleColorNmae()
+        {
+            MethodsForMulticolour obj = new MethodsForMulticolour(driver);
+            obj.Input2("y");
+        }
+
+        public void ScrollDown(int yOffset)
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript($"window.scrollBy(0,{yOffset});");
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+
+    public class MethodsForMulticolour
+    {
+        private IWebDriver driver;
+
+        public MethodsForMulticolour(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+         private By FillInput2 => By.Id("autoCompleteSingleInput");
+
+        public void Input2(string color)
+        {
+            ClickElement(FillInput2);
+            SendKeys(FillInput2,color);
+            
+        }
+
+        public void Input1(params string[] text)
+        {
+            IWebElement multiColorInput = driver.FindElement(By.Id("autoCompleteMultipleInput"));
+
+            //ClickElement(FillInput1);
+            multiColorInput.Click();
+            foreach(string color in text)
+            {
+               
+                multiColorInput.SendKeys(color);
+                multiColorInput.SendKeys(Keys.Enter);
+
+                // Wait for the autocomplete suggestion to appear
+               // WebDriverWait wait  = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+               // var suggestionXpath = By.XPath($"//div[@id='autoCompleteMultiple']//div[text()='{text}']");
+               // wait.Until(ExpectedConditions.ElementExists(suggestionXpath));
+                // Click on the suggestion
+                ////IWebElement suggestion = driver.FindElement(suggestionXpath);
+               // suggestion.Click();
+
+            }
+          
+        }
+
+        public void ClickElement(By by, int TimeOutInSeconds = 60)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(TimeOutInSeconds));
+            IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(by));
+            element.Click();
+        }
+        public void SendKeys(By by ,string text)
+        {
+            var element = driver.FindElement(by);
+           
+            element.SendKeys(text);
+            element.SendKeys(Keys.Enter);
+        }
+    }
+
+}
